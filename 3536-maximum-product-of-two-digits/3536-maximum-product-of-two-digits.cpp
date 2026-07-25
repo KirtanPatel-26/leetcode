f61ee0a8f1591695763949e02f583f44031a678e
Kirtan <kirtanpatel2677@gmail.com>
2026-07-25 10:13:23 +0530

    Time: 0 ms (100%), Space: 8.8 MB (54.99%) - LeetHub

---
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxProduct(int n) {
+        vector<int> v;
+
+        while(n>0){
+            int digit = n%10;
+            v.push_back(digit);
+            n /= 10;
+        }
+        sort(v.begin(),v.end());
+
+        return v[v.size()-1]*v[v.size()-2];
+    }
+};
