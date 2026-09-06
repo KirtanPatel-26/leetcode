233509c1d4e0bda2c8897350a73ab3f4887bceb7
Kirtan <kirtanpatel2677@gmail.com>
2026-09-06 11:46:02 +0530

    Time: 0 ms (100%), Space: 8.6 MB (39.73%) - LeetHub

---
+++ 0771-jewels-and-stones/0771-jewels-and-stones.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones) {
+        unordered_set<char> st;
+        int count = 0;
+
+        for(char c : jewels){
+            st.insert(c);
+        }
+
+        for(char c : stones){
+            if(st.count(c)){
+                count++;
+            }
+        }
+        return count;
+    }
+};
